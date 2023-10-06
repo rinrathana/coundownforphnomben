482dbe500fb3097fb18335e9d636ed80cbde41d3
rinrathana <rinrathana1507@gmail.com>
2023-10-06 14:16:19 +0700

    init project

---
+++ test.js
@@ -0,0 +1,19 @@
+var a = 2342;
+
+var num = 15;
+// do {
+//     num = num + 1;
+//     // console.log(num)
+// } while (num == 15);
+
+while (num == 15) {
+    // console.log(num += 1)
+}
+
+
+// for (let index = 0; index < num; index++) {
+//     console.log('index: ', index);
+    
+// }
+
+console.log('Testing')
